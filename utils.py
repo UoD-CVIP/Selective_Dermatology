8510bfe711a574ff5f5c53d55fded792b5617733
Jacob Carse <jacob.carse@gmail.com>
2020-07-17 01:21:15 +0100

    Added Get Trainer function The function to get the PyTorch Lightning Trainer

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@
 This file contains the following utility functions:
     str_to_bool - Function to convert a string to a boolean value.
     set_random_seed - Function to set random seed across the application.
+    get_trainer - Function to get the PyTorch Lightning Trainer used to train and test the model.
 """
 
 
@@ -11,6 +12,7 @@ This file contains the following utility functions:
 from argparse import ArgumentTypeError
 
 # Library Imports
+import torch
 import pytorch_lightning as pl
 
 
@@ -51,3 +53,54 @@ def set_random_seed(seed):
     """
 
     pl.seed_everything(seed)
+
+
+def get_trainer(arguments):
+    """
+    Function to get a PyTorch Lightning Trainer used for training and testing of a model.
+    :param arguments: Dictionary of arguments.
+    :return: A PyTorch Lightning Trainer.
+    """
+
+    # Gets the number of GPUs that should be used for the trainer.
+    if arguments["num_gpus"] == 0:
+        num_gpus = None
+    else:
+        if torch.cuda.device_count() < arguments["num_gpus"]:
+            num_gpus = torch.cuda.device_count()
+        else:
+            num_gpus = arguments["num_gpus"]
+
+    # Determines if a distributed backend should be used.
+    if arguments["distributed_backend"].lower() == "none" or num_gpus == 0:
+        distributed_backend = None
+    else:
+        distributed_backend = arguments["distributed_backend"].lower()
+
+    # Gets the level of precision that should be used.
+    if arguments["num_gpus"] == 0:
+        precision = 32
+    else:
+        precision = 16 if arguments["precision"] == -1 else None
+
+    # Determines if a batch size finder should be used to determine the maximum batch size.
+    batch_size = "binsearch" if arguments["batch_size"] == -1 else None
+
+    # Determins if a learning rate finder should be used to determine the optimal starting learning rate.
+    lr = "starting_lr" if arguments["starting_lr"] == -1 else False
+
+    # Sets up the TensorBoard Logger used to track training metrics.
+    tb_logger = pl.loggers.TensorBoardLogger(save_dir=arguments["tensorboard_dir"], name=arguments["experiment"])
+
+    # Sets up the EarlyStopping callback.
+    early_stopping = pl.callbacks.EarlyStopping("val_loss", arguments["min_delta"], arguments["patience"], verbose=True)
+
+    # Sets up the checkpoint callback.
+    checkpoint_callback = pl.callbacks.model_checkpoint.ModelCheckpoint("Models/", monitor="val_loss", save_top_k=1)
+
+    # Creates the PyTorch Lightning Trainer with values from arguments.
+    return pl.Trainer(distributed_backend=distributed_backend, gpus=num_gpus, precision=precision,
+                      max_epochs=arguments["max_epochs"], min_epochs=arguments["min_epochs"], logger=tb_logger,
+                      early_stop_callback=early_stopping, fast_dev_run=arguments["fast_dev_run"],
+                      num_sanity_val_steps=0, deterministic=True, auto_scale_batch_size=batch_size,
+                      auto_lr_find=lr, checkpoint_callback=checkpoint_callback)
